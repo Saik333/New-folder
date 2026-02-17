e01925c0f58cab84ba70464adbddb50cbeb541b8
Saik333 <113008416+Saik333@users.noreply.github.com>
2026-02-17 22:58:11 +0530

    Update snowflake_test.py

--- snowflake_test.py
+++ snowflake_test.py
@@ -6,10 +6,13 @@ import snowflake.connector
 
 conn = snowflake.connector.connect(
     account="ranaqbw-np18722",
-    user="5de13f72-cb16-47ec-8487-275ec596758c",
+    user="b60392b3-a8da-4fd0-8302-9ffe6a612dba",
     authenticator="oauth",
     token=os.environ["SNOWFLAKE_TOKEN"],
     role="TEST_ROLE",
 )
 print
 print(conn)
+
+a = conn.cursor().execute("SELECT CURRENT_USER(), CURRENT_ROLE();")
+print(a.fetchone())
