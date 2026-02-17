361b37fb5e47dec876296fc50698b7902cf2d33a
Saik333 <113008416+Saik333@users.noreply.github.com>
2026-02-17 14:48:34 +0530

    Update snowflake.py

--- snowflake.py
+++ snowflake.py
@@ -1,9 +1,12 @@
-import snowflake.connector
-import os
+print(os.environ["SNOWFLAKE_TOKEN"])
+# import snowflake.connector
+# import os
 
-conn = snowflake.connector.connect(
-    account="ranaqbw-np18722",
-    user="saikumar",
-    authenticator="oauth",
-    token=os.environ["SNOWFLAKE_TOKEN"],
-)
+# conn = snowflake.connector.connect(
+#     account="ranaqbw-np18722",
+#     user="saikumar",
+#     authenticator="oauth",
+#     token=os.environ["SNOWFLAKE_TOKEN"],
+# )
+# print
+# print(conn)
