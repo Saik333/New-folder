414f05ab1edad64f2c274ebf4a8df1142f2a891b
Saik333 <113008416+Saik333@users.noreply.github.com>
2026-02-17 22:13:03 +0530

    Update snowflake_test.py

--- snowflake_test.py
+++ snowflake_test.py
@@ -6,7 +6,7 @@ import snowflake.connector
 
 conn = snowflake.connector.connect(
     account="ranaqbw-np18722",
-    user="saikumar",
+    user="5de13f72-cb16-47ec-8487-275ec596758c",
     authenticator="oauth",
     token=os.environ["SNOWFLAKE_TOKEN"],
 )
